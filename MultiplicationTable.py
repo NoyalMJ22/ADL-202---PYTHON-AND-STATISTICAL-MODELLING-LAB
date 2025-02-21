fc7a7f93ffdfad655df09fd9db061d2c8553140e
Noyal Mathew Jain <88042213+NoyalMJ22@users.noreply.github.com>
2025-02-21 22:01:17 +0530

    Creating Multiplication Table

--- MultiplicationTable.py
+++ MultiplicationTable.py
@@ -1,5 +1,4 @@
 number = int(input ("Enter the number "))            
 print ("The Multiplication Table of: ",number)    
-
 for count in range(1, 11):      
    print (number, 'x', count, '=', number * count) 
